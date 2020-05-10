e759dc15b5ce6323cff5ae2621953232cc6a6ca7
jiwon_lala <s26788761@naver.com>
2020-05-10 22:22:19 +0900

    Add APIView comment

--- apiview/crud/post/urls.py
+++ apiview/crud/post/urls.py
@@ -14,3 +14,8 @@ urlpatterns = [
 
 # 이건 무슨 코드..?
 urlpatterns = format_suffix_patterns(urlpatterns)
+#1.format_suffix_patterns : 위 함수를 통해 특정 포맷을 간단하고 명확하게 참조할 수 있음(???). 필수는 아님
+#2.format_suffix_patterns : 위 함수를 이용하면 URL을 사용할 때 데이터 형식(원시 json이나 html)을 지정할 수 있음. 패턴의 모든 URL에 사용할 형식을 추가
+#3. https://github.com/KimDoKy/DjangoRestFramework-Tutorial/blob/master/doc/Django%20REST%20Framework%20-%2022.%20Format%20suffixes.md
+#위 함수는 제공된 각 URL 패턴에 추가된 형식 접미사 패턴을 포함하는 URL 패턴 List를 반환함
+#그리고 위 함수를 사용하는 경우 'format' 키워드 인수를 views.py에 정의된 함수나 클래스에 추가해야한다.
--- apiview/crud/post/views.py
+++ apiview/crud/post/views.py
@@ -7,6 +7,8 @@ from post.serializer import PostSerializer
 
 # status에 따라 직접 Response를 처리할 것
 from django.http import Http404
+
+# APIView를 상속받은 CBV 는 Response와 status를 import하여 status를 활용하여 직접 Response를 만들어 줌!!
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -19,23 +21,31 @@ from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
+
+'''
+APIView를 상속받은 CBV 사용하는 방법(쓰는 이유/의의)
+class ~~~(APIVeiw):
+    def <내가 필요로 하는 HTTP Method>:
+        Http Method로 어떻게 처리할 지 직접 정의하기
+        (Resopnse와 status를 사용하여)
+'''
 class PostList(APIView):
-    def get(self, request):
+    def get(self, request): #APIView를 상속받아 CBV를 사용할 때는 내부에서 사용할 HTTP 메소드의 이름으로 메소드(메소드의 이름)를 정의해야 함
         posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+        serializer = PostSerializer(posts, many=True) #다수의 객체/query set를 Serializer 로 보내줄때는 뒤에 "many=True" 인자를 넣어야 함!
+        return Response(serializer.data) #serializer에 있는 data를 그냥 Response로 보내준다
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    def post(self, request): #새 Post를 작성할때 내용을 다 작성하고 제출버튼을 누르면 호출되는 것이 이 post 메소드
+        serializer = PostSerializer(data=request.data) #그러므로 이 serializer에는 내가 request를 보낸 data가 직렬화가 되야한다
+        if serializer.is_valid(): #serializer를 유효성 검사를 하고 
+            serializer.save() #문제가 없으면 저장!
+            return Response(serializer.data, status=status.HTTP_201_CREATED) #문제없이 저장이 되었으면, serializer.data와 status를 같이 보내준다
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #유효성 검사 쪽에서 문제가 생기면 "HTTP_400_BAD_REQUEST"를 status에 담아 error와 함께 Response를 전송
+        '''질문! 그러면 Response의 첫번째 인자가 data에 해당하는데 어떤 의미의? data를 의미하는지 궁금'''
 
 #여기서 PostDetail 치면 자동완성되던데 왜 자동완성이 되는거지...?
 class PostDetail(APIView):
-
+    #get_object_or_404를 직접 구현한 것
     def get_object(self, pk):
         try:
             return Post.objects.get(pk=pk)
@@ -44,10 +54,10 @@ class PostDetail(APIView):
 
 
     def get(self, request, pk, format=None):
-        post = self.get_object(pk)
+        post = self.get_object(pk) #해당 pk에 해당하는 object를 가지고 와서 
         # post = get_object_or_404(Post, pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
+        serializer = PostSerializer(post) #해당 object 직렬화 한다음
+        return Response(serializer.data) #Response에 담아 리턴. 만약에 해당 pk에 해당하는 객체가 없으면 404가 리턴됨
 
     
     def put(self, request, pk, format=None):
@@ -61,6 +71,6 @@ class PostDetail(APIView):
 
     
     def delete(self, request, pk, format=None):
-        post = get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        post = get_object(pk) #pk에 해당하는 post 가지고 와서 
+        post.delete() #지우고 나서
+        return Response(status=status.HTTP_204_NO_CONTENT) #지웠으니 이제 해당하는 content 없다는 의미로 HTTP_204_NO_CONTENT 보내줌
