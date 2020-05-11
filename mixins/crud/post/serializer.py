e7fd13a839717a9becf7390f1c568243ba83bccd
jiwon_lala <s26788761@naver.com>
2020-05-11 23:55:43 +0900

    Add mixins views.py

--- mixins/crud/post/serializer.py
+++ mixins/crud/post/serializer.py
@@ -6,4 +6,4 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = '__all__'
         # fields = ('id', 'title', 'body')
-        write_only_fields('title',)
+        write_only_fields = ('title',)
