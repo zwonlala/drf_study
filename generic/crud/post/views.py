5dd15d06cbbe56e0538872802a2d4a9ad7bc5116
jiwon_lala <s26788761@naver.com>
2020-05-13 16:14:11 +0900

    Add generic initial commit

---
+++ generic/crud/post/views.py
@@ -0,0 +1,14 @@
+# from django.shortcuts import render
+from post.models import Post
+from post.serializer import PostSerializer
+from rest_framework import generics
+
+# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
